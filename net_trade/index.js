1eacd38fb301d257f0f7793e3a0d634f36985cd9
IlkinBairamov <ilkinabr@code.edu.az>
2022-07-07 18:07:50 +0400

    t

---
+++ net_trade/index.js
@@ -0,0 +1,41 @@
+var toggle=document.querySelector(".navbar-toggler")
+var sideBar=document.querySelector("#sidebarMenu")
+let menuOpen=false;
+var navIcon=document.querySelectorAll(".nav-icon")
+let isActive=false;
+var membership=document.querySelector(".membership")
+toggle.addEventListener('click',()=>{
+    if(!menuOpen){
+        sideBar.classList.add("show");
+        menuOpen=true
+    }
+    else{
+        sideBar.classList.remove("show");
+        menuOpen=false
+    }
+
+})
+console.log(navIcon);
+for (let index = 0; index < navIcon.length; index++) {
+    console.log(navIcon[0]);
+    navIcon[index].addEventListener('click',()=>{
+        if(!isActive){
+            navIcon[index].classList.add("active");
+            isActive=true;
+        }
+        else{
+            navIcon[index].classList.remove("active");
+            isActive=false;
+        }
+    })
+}
+membership.addEventListener('click',()=>{
+    if(!isActive){
+        membership.classList.add("active");
+        isActive=true;
+    }
+    else{
+        membership.classList.remove("active");
+        isActive=false;
+    }
+})
